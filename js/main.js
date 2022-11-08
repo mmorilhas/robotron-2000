ba456117db6a1a470294e6950ead3f2193d317ff
Michelle <michelle.morilhas@gmail.com>
2022-11-07 22:38:24 -0300

    atualizando estatisticas

--- js/main.js
+++ js/main.js
@@ -1,16 +1,52 @@
-const robotron = document.querySelector('#robotron');
-const controle = document.querySelectorAll('.controle-ajuste');
+const controle = document.querySelectorAll('[data-controle]');
+const estatistica = document.querySelectorAll('[data-estatistica]');
+
+const pecas = {
+  "bracos": {
+      "forca": 29,
+      "poder": 35,
+      "energia": -21,
+      "velocidade": -5
+  },
+
+  "blindagem": {
+      "forca": 41,
+      "poder": 20,
+      "energia": 0,
+      "velocidade": -20
+  },
+  "nucleos":{
+      "forca": 0,
+      "poder": 7,
+      "energia": 48,
+      "velocidade": -24
+  },
+  "pernas":{
+      "forca": 27,
+      "poder": 21,
+      "energia": -32,
+      "velocidade": 42
+  },
+  "foguetes":{
+      "forca": 0,
+      "poder": 28,
+      "energia": 0,
+      "velocidade": -2
+  }
+}
+
 
 controle.forEach((element) => {
   element.addEventListener('click', (event) =>{
-    manipulaDados(event.target.textContent, event.target.parentNode)
-    console.log(event);
+    manipulaDados(event.target.dataset.controle, event.target.parentNode)
+    atualizaEstatisticas(event.target.dataset.peca);
+
   })
 })
 
 
 function manipulaDados(operacao, controle){
-  const peca = controle.querySelector(".controle-contador");
+  const peca = controle.querySelector('[data-contador');
 
   if(operacao == '-'){
     peca.value = parseInt(peca.value) - 1
@@ -18,4 +54,13 @@ function manipulaDados(operacao, controle){
     peca.value = parseInt(peca.value) + 1
   }
 
+}
+
+
+function atualizaEstatisticas(peca){
+
+  estatistica.forEach((element) => {
+    console.log(element.dataset.estatistica);
+    element.textContent = parseInt(element.textContent) + pecas[peca][element.dataset.estatistica]
+  })
 }
